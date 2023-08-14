6d497dfffb749df1ebae683c84ed6f619695e0c4
VyacheslavTsukanov <vtsukanoff@yandex.ru>
2023-08-14 20:11:46 +0400

    fix

---
+++ MindBoxLib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MindBoxLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle("Circle",12);
+            Triangle triangle = new Triangle("Triangle",4,4,5);
+
+            Console.WriteLine(circle.CalcSquare());
+            Console.WriteLine(triangle.CalcSquare());
+        }
+    }
+}
